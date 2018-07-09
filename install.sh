612eed0e9140afd726e6ced87aa8c70983ebed12
Yamcha <jdbuelow@gmail.com>
2018-07-08 23:22:28 -0500

    token

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt --yes install libffi-dev libnacl-dev python3-dev python3-pip git
+python3 -m pip install --user discord-rewrite
+git clone https://github.com/jbuelow1/discord_server_fucker.git
+cd discord_server_fucker/
